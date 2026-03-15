82ca58663c3f9fd28c6e429d83ff91a2b0c227a4
kimon <kimon2002@snu.ac.kr>
2026-03-15 20:17:59 +0900

    Theme & FoxIdentity

---
+++ Sumdays/app/src/main/java/com/example/sumdays/theme/FoxChar.kt
@@ -0,0 +1,11 @@
+package com.example.sumdays.theme
+
+data class FoxChar (
+    val name: String,
+
+    // SUM 버튼 여우
+    val sumFoxIcon: Int,
+
+    // 평가해주는 여우
+    val commentFoxIcon: Int,
+)
---
+++ Sumdays/app/src/main/java/com/example/sumdays/theme/Theme.kt
@@ -0,0 +1,26 @@
+package com.example.sumdays.theme
+
+data class Theme(
+    val name: String,
+
+    // 주요 멘트 색상
+    val primaryColor: Int,
+
+    // 버튼 색상
+    val buttonColor: Int,
+
+    // 배경 색상 (배경 이미지가 존재하지 않을 때)
+    val backgroundColor: Int,
+
+    // 블럭 색상
+    val blockColor: Int,
+
+    // 캘린더 배경 이미지
+    val calendarBackgroundImage: Int,
+
+    // 메모 이미지
+    val memoImage: Int,
+
+    // 여우 캐릭터
+    val foxIcon: FoxChar,
+)
