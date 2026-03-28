1e85036211d14d4734308103a7682fe61589fad5
mingu <mingu822@snu.ac.kr>
2026-03-28 21:09:31 +0900

    step 1

---
+++ Sumdays/app/src/main/java/com/example/sumdays/social/SocialActivity.kt
@@ -0,0 +1,13 @@
+package com.example.sumdays.social
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sumdays.R
+
+class SocialActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_social)
+    }
+}
