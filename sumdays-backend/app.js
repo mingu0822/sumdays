85000a3dcad52b57d673eab45355c99c9dcddb50
mingu <mingu822@snu.ac.kr>
2026-04-20 06:39:39 +0900

    merge ok

--- sumdays-backend/app.js
+++ sumdays-backend/app.js
@@ -3,10 +3,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.use((req, res, next) => {
-    console.log(`[${req.method}] ${req.url} | Body: ${JSON.stringify(req.body)}`);
-    next();
-});
 app.use('/api', require('./routes'));
 
 // test: GET http://localhost:3000/
